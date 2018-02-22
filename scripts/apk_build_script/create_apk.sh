cb0e3e7b5653cb5c2ad7d97127d5a8d97f5c04c0
Shubham Kamthania <kamthaniashubham@gmail.com>
2018-02-22 11:18:12 +0530

    Ejecting expo and updating build scripts

--- scripts/apk_build_script/create_apk.sh
+++ scripts/apk_build_script/create_apk.sh
@@ -18,23 +18,26 @@ unzip ./temporary/lib/Livelygig.webgateway-0.3.jar -d ./temporary/
 
 # copy main stylesheet
 
-cp ./temporary/META-INF/resources/webjars/webgateway/0.3/stylesheets/wallet/wallet-app-main.min.css -d ~/project/livelygig/wallet/wallet-native-client/assets_for_webview/assets/stylesheets
+cp ./temporary/META-INF/resources/webjars/webgateway/0.3/stylesheets/wallet/wallet-app-main.min.css -d ./wallet-native-client/android/app/src/main/assets/stylesheets
 
 # copy theme stylesheet
-cp ./temporary/META-INF/resources/webjars/webgateway/0.3/stylesheets/wallet/themes/{wallet-main-theme-default.min.css,wallet-main-theme-light.min.css} -d ./wallet-native-client/assets_for_webview/assets/stylesheets
+cp ./temporary/META-INF/resources/webjars/webgateway/0.3/stylesheets/wallet/themes/{wallet-main-theme-default.min.css,wallet-main-theme-light.min.css} -d ./wallet-native-client/android/app/src/main/assets/stylesheets
 
 
 # copy javascripts
-cp ./temporary/META-INF/resources/webjars/webgateway/0.3/{wallet-client-opt-library.js,wallet-client-opt.js} -d ./wallet-native-client/assets_for_webview/assets/javascripts/
+cp ./temporary/META-INF/resources/webjars/webgateway/0.3/{wallet-client-opt-library.js,wallet-client-opt.js} -d ./wallet-native-client/android/app/src/main/assets/javascripts/
 
-## enter wallet-native directory
-#cd wallet-native-client/
-#
-## create bundle
-#react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./android/appassets_for_webview/assets/debug/inassets_for_webview/assets-dest ./android/app/build/intermediates/res/merged/debug
-#
-## enter android folder
-#cd android
-#
-## gradle assemble debug
+# enter wallet-native directory
+cd wallet-native-client/
+
+# create bundle
+react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
+
+# enter android folder
+cd android
+
+# gradle assemble debug
 #./gradlew assembleDebug
+
+# gradle assemble release
+./gradlew assembleRelease
--- wallet-client/src/main/scala/com/livelygig/product/walletclient/views/AccountView.scala
+++ wallet-client/src/main/scala/com/livelygig/product/walletclient/views/AccountView.scala
@@ -161,7 +161,7 @@ object AccountView {
               ^.className := "row heading-currency",
               <.div(
                 ^.className := "col-lg-8 col-md-8 col-sm-8 col-xs-4 hand-cuure-left",
-                <.label("total:")
+                <.label("totalsasas:")
               ),
               <.div(
                 ^.className := "col-lg-4 col-md-4 col-sm-4 col-xs-8 select-currency",
