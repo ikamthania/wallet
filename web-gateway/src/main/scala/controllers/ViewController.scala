8c120b3e6cc769823fb6c2915deffc55c8b8a0bc
Shubham Kamthania <kamthaniashubham@gmail.com>
2018-02-28 15:15:08 +0530

    Removing html views and fixing routes

--- web-gateway/src/main/scala/controllers/ViewController.scala
+++ web-gateway/src/main/scala/controllers/ViewController.scala
@@ -1,44 +1,18 @@
 package controllers
 
-import org.webjars.play.{ WebJarAssets, WebJarsUtil }
-import play.api.i18n.{ I18nSupport, Messages }
-import play.api.mvc.{ BaseController, ControllerComponents, RequestHeader }
+import org.webjars.play.WebJarsUtil
+import play.api.i18n.I18nSupport
+import play.api.mvc.{ BaseController, ControllerComponents }
 /**
  * Created by shubham.k on 27-02-2017.
  */
 
 class ViewController(
   val controllerComponents: ControllerComponents,
-  implicit val webJarsUtil: WebJarsUtil,
-  //  val messages: Messages,
-  //  val requestHeader: RequestHeader,
-  implicit val webJarsAssets: WebJarAssets)
+  implicit val webJarsUtil: WebJarsUtil)
   extends BaseController with I18nSupport {
 
   def wallet() = Action { implicit request =>
-    //    request.identity match {
-    //      case Some(identity) =>
-    //        userCache.getUserProfile(identity.userUri)
-    //          .map {
-    //            case Some(userProfile) => Redirect(controllers.routes.ViewController.walletApp(userProfile.emailOrEthAddress))
-    //            case None => Ok(views.html.wallet.landingview())
-    //          }
-    //      case None => Future.successful(Ok(views.html.wallet.landingview()))
-    //    }
-    val messages: Messages = request.messages
-    val message: RequestHeader = request
-    Ok(views.html.wallet.landingview(message, messages, webJarsAssets))
-
-  }
-
-  //  def wallet() = Action { implicit request =>
-  //    Ok(views.html.wallet.wallet(webJarsUtil))
-  //  }
-
-  def bundleUrl(projectName: String): Option[String] = {
-    val name = projectName.toLowerCase
-    Seq(s"$name-opt.js", s"$name-fastopt.js")
-      .find(name => getClass.getResource(s"/public/$name") != null)
-      .map(controllers.routes.Assets.versioned(_).url)
+    Ok(views.html.wallet.wallet(webJarsUtil))
   }
 }
