66ee19161c8a2c0bf18e1b2439965e3352e5b854
ikamthania <shubham.kamthania@afourtech.com>
2018-03-07 09:09:27 +0530

    Modified backupaccount view

--- wallet-client/src/main/scala/com/livelygig/product/walletclient/views/BackupAccountTerms.scala
+++ wallet-client/src/main/scala/com/livelygig/product/walletclient/views/BackupAccountTerms.scala
@@ -4,13 +4,15 @@ import com.livelygig.product.walletclient.facades._
 import com.livelygig.product.walletclient.facades.jquery.JQueryFacade.imports.jQuery
 import com.livelygig.product.walletclient.rootmodel.ERCTokenRootModel
 import com.livelygig.product.walletclient.router.ApplicationRouter.Loc
-import diode.data.Pot
-import diode.react.ModelProxy
+import org.scalajs.dom
+import com.livelygig.product.walletclient.router.ApplicationRouter.{ ConfirmBackupPhraseLoc, Loc }
+import com.livelygig.product.walletclient.router.ApplicationRouter.{ Loc, ViewBackupPhraseLoc }
+
+import com.livelygig.product.walletclient.router.ApplicationRouter.{ Loc, ViewBackupPhraseLoc }
 import japgolly.scalajs.react
 import japgolly.scalajs.react.extra.router.RouterCtl
 import japgolly.scalajs.react.vdom.html_<^.{ <, VdomAttr, VdomElement, ^, _ }
 import japgolly.scalajs.react.{ BackendScope, Callback, ReactEventFromHtml, ScalaComponent }
-import org.scalajs.dom
 
 object BackupAccountTerms {
 
@@ -49,7 +51,7 @@ object BackupAccountTerms {
 
       Callback.empty
     }
-    def onBtnClicked(id: String) = Callback {
+    /* def onBtnClicked(id: String) = Callback {
       id match {
         case "btnNextStart" => {
           jQuery(s".backupView-start").hide()
@@ -67,10 +69,55 @@ object BackupAccountTerms {
 
         }
       }
+    }*/
+
+    def onBtnClicked(): react.Callback = {
+      t.props.flatMap(_.router.set(ViewBackupPhraseLoc))
     }
 
     def render(p: Props, s: State): VdomElement = {
       <.div(
+        <.div(
+          ^.className := "wallet-inner-navigation",
+          <.div(
+            ^.className := "row",
+            <.div(
+              ^.className := "col-lg-12 col-md-12 col-sm-12 col-xs-12",
+              <.div(
+                ^.className := "wallet-information",
+                <.h2("Wallet"),
+                <.h3("Back up account"))))),
+        <.div(
+          ^.className := "backupView-main scrollableArea",
+          <.div(
+            ^.className := "backupView-start",
+            <.ul(
+              <.li("On the next screen you'll see a 12 word mnemonic phrase that corresponds to your new account."),
+              <.li("This is your account’s Backup Phrase."),
+              <.li("Later, you can enter this phrase into Ubunda (or other wallets) to re-gain control over your account."),
+              <.li("Make sure you are in a private place and not being watched."),
+              <.li("Write down the exact Backup Phrase and keep it in a secure location."),
+              <.li("Do not take a picture or screenshot, since that might be easily copied."),
+              <.li("Anyone with access to your Backup Phrase can gain access to your account's funds."),
+              <.li("If you fail to maintain a copy of this phrase or lose access to this wallet, you will permanently lose access to the funds in this account."))),
+
+          <.div(
+            ^.className := "backupView-generate",
+            <.div(
+              ^.className := "container btnDefault-container",
+              <.div(
+                ^.className := "row",
+                <.div(
+                  ^.className := "col-lg-12 col-md-12 col-sm-12 col-xs-12",
+                  <.button(
+                    ^.id := "btnNextStart",
+                    ^.name := "btnNxt",
+                    ^.`type` := "button",
+                    VdomAttr("data-toggle") := "modal",
+                    ^.className := "btn btnDefault goupButton setdefault",
+                    ^.onClick --> onBtnClicked(),
+                    "Next")))))))
+      /* <.div(
         <.div(
           ^.className := "wallet-inner-navigation",
           <.div(
@@ -198,7 +245,7 @@ object BackupAccountTerms {
                   VdomAttr("data-toggle") := "modal",
                   ^.className := "btn btnDefault goupButton setdefault",
                   ^.onClick --> onBtnClicked("btnNextStart"),
-                  "Next"))))))
+                  "Next"))))))*/
     }
   }
 
