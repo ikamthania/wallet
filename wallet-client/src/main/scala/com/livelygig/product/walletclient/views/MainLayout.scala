bdef06ed799dd82bd6dcedf95a6a5c3ba2a8149e
Shubham Kamthania <kamthaniashubham@gmail.com>
2018-03-22 01:15:34 +0530

    Hiding header if the session is not verified

--- wallet-client/src/main/scala/com/livelygig/product/walletclient/views/MainLayout.scala
+++ wallet-client/src/main/scala/com/livelygig/product/walletclient/views/MainLayout.scala
@@ -4,6 +4,7 @@ import com.livelygig.product.walletclient.components.HeaderComponent
 import com.livelygig.product.walletclient.handler.LoginUser
 import com.livelygig.product.walletclient.router.ApplicationRouter.{ ConfirmedBackupPhraseLoc, Loc }
 import com.livelygig.product.walletclient.services.WalletCircuit
+import com.livelygig.product.walletclient.utils.SessionKeys
 import japgolly.scalajs.react.extra.router.{ Resolution, RouterCtl }
 import japgolly.scalajs.react.vdom.html_<^._
 import org.scalajs.dom
@@ -19,7 +20,7 @@ object MainLayout {
     <.div(^.className := "wallet-main")(
       <.div(^.className := "wallet-inner container-fluid")(
         if (WalletCircuit.zoomTo(_.user.isloggedIn).value) {
-          if (!sidebarNotRequiredFor.contains(r.page)) {
+          if (!sidebarNotRequiredFor.contains(r.page) || dom.window.sessionStorage.getItem(SessionKeys.isSessionVerified) != null) {
             HeaderComponent.component(HeaderComponent.Props(c, r))
           } else {
             EmptyVdom
