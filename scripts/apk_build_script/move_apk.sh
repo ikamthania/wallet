3878d2d3135f9d08a2073f4fc03cddb51b10de28
Vinay Bhagwat <vinay.bhagwat@afourtech.com>
2018-03-28 14:54:08 +0530

    Adding modifications for shell scripts

--- scripts/apk_build_script/move_apk.sh
+++ scripts/apk_build_script/move_apk.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-cp -a -fr /home/ubuntu/work/livelygig/test-deployment/wallet/wallet-native-client/android/app/build/outputs/apk/app-release.apk /home/ubuntu/work/livelygig/mobile-app/android/
+cd  /home/ubuntu/work/livelygig/test-deployment/wallet/wallet-native-client/android/app/build/outputs/apk/
+
+cp app-debug.apk /home/ubuntu/work/livelygig/mobile-app/android/
+
+
+
+
+
+
+
+
+
