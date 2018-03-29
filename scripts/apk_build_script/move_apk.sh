b2473f1ac56816de5f2f4fa4a286102be7a08fef
Vinay Bhagwat <vinay.bhagwat@afourtech.com>
2018-03-29 15:03:09 +0530

    Modifications for coreapi and move_apk.sh scripts

--- scripts/apk_build_script/move_apk.sh
+++ scripts/apk_build_script/move_apk.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd  /home/ubuntu/work/livelygig/test-deployment/wallet/wallet-native-client/android/app/build/outputs/apk/
 
-cp app-debug.apk /home/ubuntu/work/livelygig/mobile-app/android/
+cp app-debug.apk /home/ubuntu/work/livelygig/mobile-app/android/"Ubunda-$(date).apk"
 
 
 
