8cddabb5627278cfa306b61ab975becb861db461
Shubham Kamthania <kamthaniashubham@gmail.com>
2018-04-24 23:07:15 +0530

    Adding support for using caches
    Assets are added to cache on fetch
    Removed reduntant twirl templating

--- web-gateway/src/main/twirl/views/wallet/serviceworker.scala.js
+++ web-gateway/src/main/twirl/views/wallet/serviceworker.scala.js
@@ -1,18 +1,18 @@
 
 var cacheName = 'v1:static';
 
-@defining(s"wallet-client") { name =>
-@Seq(s"$name-opt-library.js", s"$name-fastopt-library.js",s"$name-opt.js", s"$name-fastopt.js").filter(asset => getClass.getResource(s"/public/$asset") != null).map(name => s"${routes.Assets.versioned(name).toString}")
-}
+@appJs = @{Seq("wallet-client-opt-library.js", "wallet-client-fastopt-library.js","wallet-client-opt.js", "wallet-client-fastopt.js").filter(asset => getClass.getResource(s"/public/$asset") != null).map(name => s"${routes.Assets.versioned(name).toString}")}
 
 self.addEventListener('install', function (e) {
     // once the SW is installed, go ahead and fetch the resources to make this work offline
     e.waitUntil(
         caches.open(cacheName).then(function (cache) {
             return cache.addAll([
+                '../wallet',
                 './manifest.json',
-                "https://fonts.googleapis.com/css?family=Montserrat",
+
                 './serviceworker.js',
+                "https://fonts.googleapis.com/css?family=Montserrat",
                 '@routes.Assets.versioned("stylesheets/wallet/wallet-app-main.min.css")',
                 "@routes.Assets.versioned("stylesheets/wallet/themes/wallet-main-theme-default.min.css")",
                 "@routes.Assets.versioned("stylesheets/wallet/themes/wallet-main-theme-light.min.css")",
